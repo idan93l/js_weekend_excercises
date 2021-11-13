b1a14f85beefc38f619f3c71e8226c0ac41cc0f1
idan.93l <idan.93lev@gmail.com>
2021-11-13 16:01:19 +0200

    Tribonacci

---
+++ 4.2.js
@@ -0,0 +1,15 @@
+function tribonacci(signature,n) {
+  const empty = [];
+  if(n === 0) {
+    return empty;
+  }
+  let triArr = signature;
+  for (i = 2; i < n; i++) {
+    triArr.push((triArr[i] + triArr[i-1] + triArr[i-2]));
+  }
+  return triArr.slice(0, n);
+}
+
+console.log(tribonacci([0, 0, 1], 8))
+console.log(tribonacci([1, 1, 1,], 8));
+console.log(tribonacci([1, 1, 1,], 0));
