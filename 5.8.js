fc1f047fa9bef0719661f0a729140ed19c7d56f3
idan.93l <idan.93lev@gmail.com>
2021-11-14 10:54:03 +0200

    Longest words

--- 5.8.js
+++ 5.8.js
@@ -1,12 +1,18 @@
 const short = str => {
   let newStr = str.split(` `);
   let longestLength = newStr[0].length;
+  let longestWords = [];
   for (let i = 0; i < newStr.length; i++) {
     if (newStr[i].length > longestLength) {
       longestLength = newStr[i].length;
     }
-}
-  return longestLength;
+  }
+  for(let j = 0; j < newStr.length; j++) {
+    if (newStr[j].length === longestLength) {
+      longestWords.push(newStr[j])
+    }
+  }
+  return longestWords;
 }
 
 console.log(short(`I am iron man`));
